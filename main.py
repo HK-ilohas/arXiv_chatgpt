8c1d01cf279980291a0758609f187c8a1d217895
Koki Hamada <hamadakoki39@gmail.com>
2024-04-02 22:22:53 +0900

    refactored

--- main.py
+++ main.py
@@ -16,122 +16,121 @@ from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_text_splitters import CharacterTextSplitter
 
-model = ChatOpenAI(
-    model_name="gpt-3.5-turbo",
-)
+
+model = ChatOpenAI(model_name="gpt-3.5-turbo")
 embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
 
-retriever = ArxivRetriever(load_max_docs=1)
 
-map_template = """以下の文章を要約してください。
-ただし、以下のルールに従ってください。
+def initialize_chains():
+    map_template = """
+    以下の文章を要約してください。
+    ただし、次のことに注意してください。
+    - 簡潔に表現する
+    - 不明な単語や人名と思われるものは英語のまま表示する
+    
+    入力:
+    {docs}
 
-- 簡潔に表現する
-- 不明な単語や人名と思われるものは英語のまま表示する
+    要約:
+    """
+    map_prompt = PromptTemplate.from_template(map_template)
+    map_chain = LLMChain(llm=model, prompt=map_prompt)
 
-それでは開始します。:
+    reduce_template = """
+    以下は一連の文書です。日本語で要約してください。
+    ただし、次のことに注意してください。
+    - 簡潔に表現する
+    - 不明な単語や人名と思われるものは英語のまま表示する
+    
+    入力:
+    {docs}
 
-{docs}
+    要約:
+    """
+    reduce_prompt = PromptTemplate.from_template(reduce_template)
+    reduce_chain = LLMChain(llm=model, prompt=reduce_prompt)
+    combine_documents_chain = StuffDocumentsChain(
+        llm_chain=reduce_chain, document_variable_name="docs"
+    )
 
-要約:"""
-map_template = PromptTemplate.from_template(map_template)
-map_chain = LLMChain(llm=model, prompt=map_template)
+    reduce_documents_chain = ReduceDocumentsChain(
+        combine_documents_chain=combine_documents_chain,
+        collapse_documents_chain=combine_documents_chain,
+        token_max=4000,
+    )
 
-# Reduce
-reduce_template = """以下は一連の文書です:
+    map_reduce_chain = MapReduceDocumentsChain(
+        llm_chain=map_chain,
+        reduce_documents_chain=reduce_documents_chain,
+        document_variable_name="docs",
+        return_intermediate_steps=False,
+    )
 
-{docs}
-
-このドキュメントのリストに基づいて、簡潔にわかりやすく日本語で要約してください。
-ただし、以下のルールに従ってください。
-
-- 簡潔に表現する
-- 不明な単語や人名と思われるものは英語のまま表示する
-
-要約:"""
-reduce_prompt = PromptTemplate.from_template(reduce_template)
-
-# Run chain
-reduce_chain = LLMChain(llm=model, prompt=reduce_prompt)
-
-# Takes a list of documents, combines them into a single string, and passes this to an LLMChain
-combine_documents_chain = StuffDocumentsChain(
-    llm_chain=reduce_chain, document_variable_name="docs"
-)
-
-# Combines and iteratively reduces the mapped documents
-reduce_documents_chain = ReduceDocumentsChain(
-    # This is final chain that is called.
-    combine_documents_chain=combine_documents_chain,
-    # If documents exceed context for `StuffDocumentsChain`
-    collapse_documents_chain=combine_documents_chain,
-    # The maximum number of tokens to group documents into.
-    token_max=4000,
-)
-
-# Combining documents by mapping a chain over them, then combining results
-map_reduce_chain = MapReduceDocumentsChain(
-    # Map chain
-    llm_chain=map_chain,
-    # Reduce chain
-    reduce_documents_chain=reduce_documents_chain,
-    # The variable name in the llm_chain to put the documents in
-    document_variable_name="docs",
-    # Return the results of the map steps in the output
-    return_intermediate_steps=False,
-)
-
-chat_history = []
+    return map_reduce_chain
 
 
-@cl.on_chat_start
-async def chat_start():
+async def handle_chat_start():
+    retriever = ArxivRetriever(load_max_docs=1)
     docs = None
+
     while docs is None:
         res = await cl.AskUserMessage("arXivのURLを入力してください！").send()
         if res is None:
             continue
         url = res["output"]
-        if not (
-            url.startswith("https://arxiv.org/abs/")
-            or url.startswith("https://arxiv.org/pdf/")
-            or url.startswith("http://arxiv.org/abs/")
-            or url.startswith("http://arxiv.org/pdf/")
+        if not url.startswith(
+            (
+                "https://arxiv.org/abs/",
+                "https://arxiv.org/pdf/",
+                "http://arxiv.org/abs/",
+                "http://arxiv.org/pdf/",
+            )
         ):
             await cl.Message(
                 "URLが正しい形式ではありません。`https://arxiv.org/abs/`または`https://arxiv.org/pdf/`の形式で入力してください。"
             ).send()
-        else:
-            paper_id = url.split("/")[-1]
-            if paper_id.endswith(".pdf"):
-                paper_id = paper_id[:-4]
-            docs = retriever.get_relevant_documents(query=paper_id)
+            continue
+
+        paper_id = url.split("/")[-1].replace(".pdf", "")
+        docs = retriever.get_relevant_documents(query=paper_id)
+
+    return docs
+
+
+async def process_document(docs):
     id = docs[0].metadata["Entry ID"].split("/")[-1]
     pdf_url = f"https://arxiv.org/pdf/{id}.pdf"
     response = requests.get(pdf_url)
+
     if not os.path.exists("tmp"):
         os.mkdir("tmp")
+
     with open(f"tmp/{id}.pdf", "wb") as f:
         f.write(response.content)
+
     pdf_docs = PyMuPDFLoader(f"tmp/{id}.pdf").load()
     text_splitter = CharacterTextSplitter.from_tiktoken_encoder(
         chunk_size=1000, chunk_overlap=0
     )
     split_docs = text_splitter.split_documents(pdf_docs)
+
+    return split_docs
+
+
+@cl.on_chat_start
+async def chat_start():
+    docs = await handle_chat_start()
+    split_docs = await process_document(docs)
+
     database = Chroma(embedding_function=embeddings)
     database.add_documents(split_docs)
     cl.user_session.set("database", database)
 
+    map_reduce_chain = initialize_chains()
     res = map_reduce_chain.invoke(split_docs)
 
-    msg = f"""## 論文
-タイトル: {docs[0].metadata['Title']}
-著者: {docs[0].metadata['Authors']}
-## 要約
-{res['output_text']}"""
-
+    msg = f"## 論文\nタイトル: {docs[0].metadata['Title']}\n著者: {docs[0].metadata['Authors']}\n## 要約\n{res['output_text']}"
     await cl.Message(msg).send()
-    return
 
 
 @cl.on_message
@@ -140,6 +139,10 @@ async def main(message):
     pdf_qa = ConversationalRetrievalChain.from_llm(
         model, database.as_retriever(), return_source_documents=True
     )
+
     result = pdf_qa({"question": message.content, "chat_history": chat_history})
     chat_history.append((message.content, result["answer"]))
     await cl.Message(result["answer"]).send()
+
+
+chat_history = []
